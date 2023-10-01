942d34c95ae8a0adf0442744f860a14a2021192e
vignesh.bandla <vignesh.bandla01@gmail.com>
2023-10-01 10:05:02 +0530

    UPDATE : added the basic project structre

---
+++ expense-manager/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { ExpenseManger } from "./Components/ExpenseManager";
+
+function App() {
+  return (
+    <div className="App">
+      <ExpenseManger></ExpenseManger>
+    </div>
+  );
+}
+
+export default App;
---
+++ expense-manager/src/Components/ExpenseManager.jsx
@@ -0,0 +1,7 @@
+export function ExpenseManger() {
+  return (
+    <div id="home">
+      <h1>Expense Manager</h1>
+    </div>
+  );
+}
